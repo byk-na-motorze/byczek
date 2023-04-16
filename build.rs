c3b05b438775042eae9850b82b89f4ad664edb4b
Byk Na Motorze <lucasfirstthegreat@gmail.com>
2023-04-17 00:36:32 +0800

    proto updates

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/auth.proto")?;
+    tonic_build::compile_protos("proto/comment.proto")?;
     tonic_build::compile_protos("proto/email.proto")?;
+    tonic_build::compile_protos("proto/image.proto")?;
+    tonic_build::compile_protos("proto/meme.proto")?;
+    tonic_build::compile_protos("proto/notification.proto")?;
+    tonic_build::compile_protos("proto/profile.proto")?;
+    tonic_build::compile_protos("proto/votes.proto")?;
     Ok(())
 }
